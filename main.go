b09cca96d1de3c8a46932d158964dea2259d1837
Dave <its-dave@users.noreply.github.com>
2022-09-08 10:44:46 +0100

    refactor

--- main.go
+++ main.go
@@ -44,55 +44,9 @@ func Mux() *http.ServeMux {
 				return
 			}
 
-			// Parse request body
-			body := body(r)
-			var bodyJson interface{}
-			err = json.Unmarshal(body, &bodyJson)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-			bodyMap := bodyJson.(map[string]interface{})
-			if len(bodyMap) != 1 {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-
-			dataMap := storedData()
-
-			// Set new key:value
-			for key, valueInterface := range bodyMap {
-				value, ok := valueInterface.(string)
-				if !ok {
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				}
-
-				if _, exists := dataMap[key]; exists {
-					// TODO: if key exists but has no value then append to array
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				}
-				dataMap[key] = []eventObj{
-					{
-						Event: "create",
-						Value: value,
-					},
-				}
-			}
-
-			// Write data
-			dataToWrite, err := json.Marshal(dataMap)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			if err := os.WriteFile(dataFilePath, dataToWrite, 0666); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-			w.WriteHeader(http.StatusAccepted)
+			respBody, respCode := handleCreateReq(r)
+			w.WriteHeader(respCode)
+			fmt.Fprint(w, respBody)
 			return
 		case 2:
 			// TODO: /api/key is called, must not be POST
@@ -100,37 +54,9 @@ func Mux() *http.ServeMux {
 			case http.MethodGet:
 				// Get value for key
 
-				key := urlParts[1]
-				dataMap := storedData()
-				keyArray, exists := dataMap[key]
-
-				// Key does not exist
-				if !exists {
-					w.WriteHeader(http.StatusNotFound)
-					return
-				}
-
-				// Get value into a usable form
-				array, ok := keyArray.([]interface{})
-				if !ok {
-					w.WriteHeader(http.StatusInternalServerError)
-					return
-				}
-				latest := array[len(array)-1]
-				latestJson, _ := json.Marshal(latest)
-				var latestEventObj eventObj
-				err = json.Unmarshal(latestJson, &latestEventObj)
-				if err != nil {
-					w.WriteHeader(http.StatusInternalServerError)
-					return
-				}
-
-				// Key has been deleted
-				if latestEventObj.Value == "" {
-					w.WriteHeader(http.StatusNoContent)
-					return
-				}
-				fmt.Fprint(w, latestEventObj.Value)
+				respBody, respCode := handleReadReq(r, urlParts[1])
+				w.WriteHeader(respCode)
+				fmt.Fprint(w, respBody)
 				return
 			case http.MethodPatch, http.MethodPut:
 				// TODO: if key doesn't exist: 404
@@ -156,21 +82,102 @@ func Mux() *http.ServeMux {
 	return mux
 }
 
+// handleCreateReq handles a post request and returns the desired response body and code
+func handleCreateReq(r *http.Request) (string, int) {
+	// Parse request body
+	body := body(r)
+	var bodyJson interface{}
+	err := json.Unmarshal(body, &bodyJson)
+	if err != nil {
+		return "TODO: invalid post body", http.StatusBadRequest
+	}
+	bodyMap := bodyJson.(map[string]interface{})
+	if len(bodyMap) != 1 {
+		return "TODO: invalid post body", http.StatusBadRequest
+	}
+
+	dataMap, err := storedData()
+	if err != nil {
+		return err.Error(), http.StatusInternalServerError
+	}
+
+	// Set new key:value
+	for key, valueInterface := range bodyMap {
+		value, ok := valueInterface.(string)
+		if !ok {
+			return "TODO: invalid post body", http.StatusBadRequest
+		}
+
+		if _, exists := dataMap[key]; exists {
+			// TODO: if key exists but has no value then append to array
+			return "TODO: key already exists", http.StatusBadRequest
+		}
+		dataMap[key] = []eventObj{
+			{
+				Event: "create",
+				Value: value,
+			},
+		}
+	}
+
+	// Write data
+	dataToWrite, err := json.Marshal(dataMap)
+	if err != nil {
+		return err.Error(), http.StatusInternalServerError
+	}
+	if err := os.WriteFile(dataFilePath, dataToWrite, 0666); err != nil {
+		return err.Error(), http.StatusInternalServerError
+	}
+	return "", http.StatusAccepted
+}
+
+// handleReadReq handles a get request and returns the desired response body and code
+func handleReadReq(r *http.Request, key string) (string, int) {
+	dataMap, err := storedData()
+	if err != nil {
+		return err.Error(), http.StatusInternalServerError
+	}
+	keyArray, exists := dataMap[key]
+
+	// Key does not exist
+	if !exists {
+		return "", http.StatusNotFound
+	}
+
+	// Get value into a usable form
+	array, ok := keyArray.([]interface{})
+	if !ok {
+		return err.Error(), http.StatusInternalServerError
+	}
+	latest := array[len(array)-1]
+	latestJson, _ := json.Marshal(latest)
+	var latestEventObj eventObj
+	err = json.Unmarshal(latestJson, &latestEventObj)
+	if err != nil {
+		return err.Error(), http.StatusInternalServerError
+	}
+
+	// Key has been deleted
+	if latestEventObj.Value == "" {
+		return "", http.StatusNoContent
+	}
+
+	return latestEventObj.Value, http.StatusOK
+}
+
 // storedData parses the stored JSON data and returns it as a map
-func storedData() map[string]interface{} {
+func storedData() (map[string]interface{}, error) {
 	// Parse stored data
 	data, err := os.ReadFile(dataFilePath)
 	if err != nil {
-		// TODO: 500
-		return nil
+		return nil, err
 	}
 	var jsonData interface{}
 	err = json.Unmarshal(data, &jsonData)
 	if err != nil {
-		// TODO: 500
-		return nil
+		return nil, err
 	}
-	return jsonData.(map[string]interface{})
+	return jsonData.(map[string]interface{}), nil
 }
 
 func body(r *http.Request) []byte {
